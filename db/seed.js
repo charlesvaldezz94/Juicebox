b18246c61007938446827fe390e529855926f772
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-10-26 11:06:51 -0500

    we are on seeding now

--- db/seed.js
+++ db/seed.js
@@ -3,6 +3,7 @@ const {
     getAllUsers // new
   } = require('./index');
   
+
   async function testDB() {
     try {
       client.connect();
@@ -16,4 +17,40 @@ const {
     }
   }
 
-  testDB();
+  
+  async function dropTables() {
+    try {
+      await client.query(`
+  
+      `);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+  async function createTables() {
+    try {
+      await client.query(`
+  
+      `);
+    } catch (error) {
+      throw error; // we pass the error up to the function that calls createTables
+    }
+  }
+
+
+  async function rebuildDB() {
+    try {
+      client.connect();
+  
+      await dropTables();
+      await createTables();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      client.end();
+    }
+  }
+
+  rebuildDB();
