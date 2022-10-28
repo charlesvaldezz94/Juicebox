d663fafad177c02180c0396688b84d880abd885f
Randy Pellegrin <rpell391@gmail.com>
2022-10-28 12:36:37 -0500

    completed step 3

--- db/seed.js
+++ db/seed.js
@@ -11,6 +11,7 @@ const {
   getUserById,
   addTagsToPost,
   getPostById,
+  getPostsByTagName,
 } = require("./index");
 
 async function dropTables() {
@@ -185,6 +186,15 @@ async function testDB() {
     console.log("Checking the tags");
     const tagcheck = await getPostById(1);
     console.log("TAGS:", tagcheck);
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"],
+    });
+    console.log("Result:", updatePostTagsResult);
+
+    console.log("Calling getPostsByTagName with #happy");
+    const postsWithHappy = await getPostsByTagName("#happy");
+    console.log("Result:", postsWithHappy);
 
     console.log("Finished database tests!");
   } catch (error) {
