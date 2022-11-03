23e89a7fea708ee8c1675b93e524e18da3e7cb33
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-11-03 14:27:16 -0500

    getpostsbytagname working on currently

--- api/tags.js
+++ api/tags.js
@@ -11,18 +11,8 @@ tagsRouter.use((req, res, next) => {
 tagsRouter.get("/:tagName/posts", async (req, res, next) => {
   // read the tagname from the params
   try {
-const { rows: postIds } = await client.query(
-      `
-      SELECT posts.id
-      FROM posts
-      JOIN post_tags ON posts.id=post_tags."postId"
-      JOIN tags ON tags.id=post_tags."tagId"
-      WHERE tags.name=$1;
-    `,
-      [tagName]
-    );
-
-    return await Promise.all(postIds.map((post) => getPostById(post.id)));
+    const posts = await getPostsByTagName
+    res.send ({ posts })
     // send out an object to the client { posts: // the posts }
   } catch ({ name, message }) {
     next({ name, message });
